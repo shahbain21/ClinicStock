426ff351b38a3cda6d2b63618c6ea849d989be23
Mohamed Shahbain <shahbain213@gmail.com>
2026-04-06 09:11:05 -0400

    Removed untitled

--- ClinicStock/ClinicStockApp.swift
+++ ClinicStock/ClinicStockApp.swift
@@ -8,28 +8,25 @@
 import SwiftUI
 import FirebaseCore
 
-
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         FirebaseApp.configure()
-        print("Firebase is connected!")
+        print("Firebase connected")
         return true
     }
 }
 
 @main
 struct ClinicStockApp: App {
-    
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var authManager = AuthManager()
     @StateObject var inventoryManager = InventoryManager()
-
+    
     var body: some Scene {
         WindowGroup {
-            // ContentView()
             RootView()
                 .environmentObject(authManager)
                 .environmentObject(inventoryManager)
--- Untitled.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Untitled.swift
-//  ClinicStock
-//
-//  Created by Mohamed Shahbain on 4/2/26.
-//
-
